8aee71338bf07829e5bece84b83b8baf82895209
Kartikeyasthana9 <harshitasthana9@gmail.com>
2022-09-19 09:26:22 +0530

    bog

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -25,7 +25,7 @@ const NavBar = () => {
                     {/* Left links */}
                     <ul className="navbar-nav mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
+                            <Nav className="nav-link active" aria-current="page" href="#">
                                 Home
                             </a>
                         </li>
